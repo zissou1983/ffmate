1381542b945d48123c55bba3c58ca5d3d758359c
YoSev <severinsiebertz@googlemail.com>
2025-01-10 12:54:10 +0100

    feat: add DATE_WEEK wildcard

--- pkg/ffmpeg/wildcards.go
+++ pkg/ffmpeg/wildcards.go
@@ -15,6 +15,9 @@ func evaluateWildcards(request *ExecutionRequest) {
 	request.Command = strings.ReplaceAll(request.Command, "${DATE_MONTH}", time.Now().Format("01"))
 	request.Command = strings.ReplaceAll(request.Command, "${DATE_DAY}", time.Now().Format("02"))
 
+	_, week := time.Now().ISOWeek()
+	request.Command = strings.ReplaceAll(request.Command, "${DATE_WEEK}", strconv.Itoa(week))
+
 	request.Command = strings.ReplaceAll(request.Command, "${TIME_HOUR}", time.Now().Format("15"))
 	request.Command = strings.ReplaceAll(request.Command, "${TIME_MINUTE}", time.Now().Format("04"))
 	request.Command = strings.ReplaceAll(request.Command, "${TIME_SECOND}", time.Now().Format("05"))
@@ -23,4 +26,5 @@ func evaluateWildcards(request *ExecutionRequest) {
 	request.Command = strings.ReplaceAll(request.Command, "${TIMESTAMP_MILLISECONDS}", strconv.FormatInt(time.Now().UnixMilli(), 10))
 	request.Command = strings.ReplaceAll(request.Command, "${TIMESTAMP_MICROSECONDS}", strconv.FormatInt(time.Now().UnixMicro(), 10))
 	request.Command = strings.ReplaceAll(request.Command, "${TIMESTAMP_NANOSECONDS}", strconv.FormatInt(time.Now().UnixNano(), 10))
+
 }
