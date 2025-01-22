fa3c58a52c26edf95118a2a34ca4e233875ea002
YoSev <severinsiebertz@googlemail.com>
2025-01-22 08:56:31 +0100

    feat: update ui

---
+++ cmd/reset.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/welovemedia/ffmate/internal/config"
+	"github.com/welovemedia/ffmate/internal/database/model"
+	"github.com/welovemedia/ffmate/sev"
+)
+
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "reset running jobs",
+	Run:   reset,
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+	resetCmd.PersistentFlags().StringP("database", "", "db.sqlite", "the path do the database")
+	viper.BindPFlag("database", resetCmd.PersistentFlags().Lookup("database"))
+	rootCmd.AddCommand(resetCmd)
+}
+
+func reset(cmd *cobra.Command, args []string) {
+	s := sev.New("ffmate", config.Config().AppVersion, config.Config().Database, 0)
+	s.DB().Model(&model.Task{}).
+		Where("status = ?", "RUNNING").
+		Updates(map[string]interface{}{
+			"status":   "DONE_CANCELED",
+			"progress": 100,
+		})
+	s.Logger().Infof("All running jobs have been reset")
+}
