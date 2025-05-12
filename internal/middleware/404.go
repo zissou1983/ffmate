622a904c0364a1f13f1755ae925597b75d55be41
YoSev <severinsiebertz@googlemail.com>
2025-05-12 10:57:53 +0200

    test: add middleware tests

--- internal/middleware/404.go
+++ internal/middleware/404.go
@@ -5,7 +5,7 @@ import (
 	"github.com/welovemedia/ffmate/sev"
 )
 
-func E404(c *gin.Context, s *sev.Sev) {
+func E404(c *gin.Context, _ *sev.Sev) {
 	if c.Writer.Status() == 404 {
 		c.AbortWithStatus(404)
 		return
